bbbd2af172b47a54e155bcf72b7785dfd6a03338
Sam Wu <sjwu@ualberta.ca>
2017-08-08 21:32:09 -0700

    where clause

---
+++ where.sql
@@ -0,0 +1,11 @@
+--Where
+
+
+/*
+syntax
+SELECT columnA, columnB, ... FROM table WHERE condition
+can also be used with UPDATE, DELETE, etc.
+quotes for text fields, unquoted for numeric fields
+*/
+SELECT * FROM table WHERE name='Sam';
+SELECT * FROM table WHERE ID=1;
