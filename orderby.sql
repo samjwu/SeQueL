700ae234bf5fd004e8932db71bb83832c080cfb7
Sam Wu <sjwu@ualberta.ca>
2017-08-10 21:44:18 -0700

    order by keyword

---
+++ orderby.sql
@@ -0,0 +1,16 @@
+--Order By
+
+
+/*
+sorts result-set in ascending order by default
+syntax
+SELECT columnA, columnB, ... FROM table ORDER BY columnA, columnB, ... ASC|DESC
+*/
+SELECT * FROM table ORDER BY ID;
+SELECT * FROM table ORDER BY name DESC;
+
+/*
+sort by number, then by name (both ASC by default)
+*/
+SELECT * FROM table ORDER BY num, name;
+SELECT * FROM table ORDER BY num ASC, name DESC;
