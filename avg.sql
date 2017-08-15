8fdd5c704173ecb41a530ab60bc783d20f8307ed
Sam Wu <sjwu@ualberta.ca>
2017-08-14 21:57:18 -0700

    avg/sum fxns

---
+++ avg.sql
@@ -0,0 +1,8 @@
+--Avg
+
+
+/*
+syntax
+SELECT AVG(col) FROM table WHERE cond
+*/
+SELECT AVG(ID) FROM table;
---
+++ sum.sql
@@ -0,0 +1,8 @@
+--Sum
+
+
+/*
+syntax
+SELECT SUM(col) FROM table WHERE cond
+*/
+SELECT SUM(ID) FROM table;
