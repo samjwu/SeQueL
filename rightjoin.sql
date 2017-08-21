50ccd9b1d5c5d25cb6e3d243eb3471e10fa38106
Sam Wu <sjwu@ualberta.ca>
2017-08-20 21:26:22 -0700

    right join

---
+++ rightjoin.sql
@@ -0,0 +1,16 @@
+--Right Join (Right Outer Join)
+
+
+/*
+Returns all records from right table and matching records from the left table
+http://www.dofactory.com/Images/sql-joins.png
+
+syntax
+SELECT colA, colB, ... FROM tableA RIGHT JOIN tableB ON cond
+*/
+
+/*
+All students from the left table and students with the same names from the right table
+*/
+SELECT table.ID, secondtable.gender FROM table RIGHT JOIN secondtable ON table.name=secondtable.name
+ORDER BY table.name;
