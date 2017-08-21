af361adb9d1ace405863a66825f88a36f42d5219
Sam Wu <sjwu@ualberta.ca>
2017-08-20 21:31:23 -0700

    full outer join

--- rightjoin.sql
+++ rightjoin.sql
@@ -10,7 +10,7 @@ SELECT colA, colB, ... FROM tableA RIGHT JOIN tableB ON cond
 */
 
 /*
-All students from the left table and students with the same names from the right table
+All students from the right table and students with the same names from the left table
 */
 SELECT table.ID, secondtable.gender FROM table RIGHT JOIN secondtable ON table.name=secondtable.name
 ORDER BY table.name;
